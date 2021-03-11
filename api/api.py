6437df2b458b563a4ddfd418f0b437eecf05279c
Sharan Arkalgud <sharkalgud@fb.com>
2021-03-11 14:06:03 -0500

    added functionality to read from s3 bucket and return all file text

--- api/api.py
+++ api/api.py
@@ -1,8 +1,13 @@
 import time
 from flask import Flask
+from search import all_file_text
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/s3_files')
+def get_text_in_s3_files():
+    return all_file_text()
---
+++ api/search.py
@@ -0,0 +1,33 @@
+import boto3
+import os
+
+BUCKET = "seed-serverless-notes-api-tutorial-file-storage"
+DOWNLOADDIR = "files/downloads/"
+
+#downloads all files present in s3 bucket
+#returns keys in downloads
+def download_files():
+    s3Client = boto3.client('s3')
+    s3Resource = boto3.resource('s3')
+    keys = []
+    response = s3Client.list_objects(Bucket=BUCKET)
+    if 'Contents' in response:
+        for item in response['Contents']:
+            keys.append(item['Key'])
+    for key in keys:
+        print(DOWNLOADDIR + key)
+        s3Resource.meta.client.download_file(BUCKET, key, DOWNLOADDIR + key)
+    return keys
+
+#returns all the text for files in download/files in dict
+def all_file_text():
+    download_files()
+
+    file_text = {}
+    directory = os.fsencode(DOWNLOADDIR)
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        with open(DOWNLOADDIR + filename) as f:
+            text = f.read()
+        file_text[filename] = text
+    return file_text
