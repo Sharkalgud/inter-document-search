a9f9b87d4dd681cf67dc5b05fabb289ba0a6e1b4
Sharan Arkalgud <sharkalgud@fb.com>
2021-03-12 11:50:13 -0500

    refactored api to swap out search methods

--- api/api.py
+++ api/api.py
@@ -1,6 +1,6 @@
 import time
 from flask import Flask, request
-from search import all_file_text
+from search import all_file_text, extactMatchSearch
 
 app = Flask(__name__)
 
@@ -31,20 +31,6 @@ def search():
 @app.route('/search2')
 def search2():
     search_term = request.args.get('term').lower()
-    file_text = all_file_text()
-    search_results = {}
     if search_term == '':
-        return search_results
-    for file_name in file_text.keys():
-        text = file_text[file_name]
-        sentenctes = text.split('. ')
-        count = 0
-        for sentence in sentenctes:
-            if search_term in sentence.lower():
-                sentence_obj = {"start": count, "length": len(sentence)}
-                if file_name in search_results:
-                    search_results[file_name]["results"].append(sentence_obj)
-                else:
-                    search_results[file_name] = {"text": text, "results": [sentence_obj]}
-            count += len(sentence) + 2
-    return search_results
+        return {}
+    return extactMatchSearch(search_term)
--- api/search.py
+++ api/search.py
@@ -31,3 +31,21 @@ def all_file_text():
                 text = f.read()
             file_text[filename] = text
     return file_text
+
+#returns positions of sentences in each document that have the search term in them
+def extactMatchSearch(search_term):
+    file_text = all_file_text()
+    search_results = {}
+    for file_name in file_text.keys():
+        text = file_text[file_name]
+        sentenctes = text.split('. ')
+        count = 0
+        for sentence in sentenctes:
+            if search_term in sentence.lower():
+                sentence_obj = {"start": count, "length": len(sentence)}
+                if file_name in search_results:
+                    search_results[file_name]["results"].append(sentence_obj)
+                else:
+                    search_results[file_name] = {"text": text, "results": [sentence_obj]}
+            count += len(sentence) + 2
+    return search_results
