449a1a1898caac3acbd4b75d33241d1f9eb2aa82
Sharan Arkalgud <sharkalgud@fb.com>
2021-03-12 00:50:26 -0500

    cleaning up old path for now

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-// import ResultsContainer from './ResultsContainer';
 import ResultsContainer2 from './ResultsContainer2';
-import Mark from "mark.js"
 
 function App() {
   const [results, setResults] = useState({});
@@ -10,16 +8,6 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    // fetch('/search?term=' + searchTerm).then(res => res.json()).then(data => {
-    //   setResults(data);
-    // }).then(() => {
-    //   var markInstance = new Mark(document.querySelectorAll("#search-result"));
-    //   markInstance.unmark({
-    //   done: () => {
-    //       markInstance.mark(searchTerm);
-    //   }
-    //   });
-    // });
     fetch('/search2?term=' + searchTerm).then(res => res.json()).then(data => {
       setResults2(data);
     });
@@ -31,7 +19,6 @@ function App() {
         <input type= 'text' value = {searchTerm} onChange = {e => setSearchTerm(e.target.value)} placeholder = 'Search for anything'/>
       </div>
       <br></br>
-      {/* <ResultsContainer results = {results}/> */}
       <ResultsContainer2 results = {results2}/>
     </div>
   );
