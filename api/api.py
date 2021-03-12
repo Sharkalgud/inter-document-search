e5b0800448bff6548ba4ff1654bc69489ad599d1
Sharan Arkalgud <sharkalgud@fb.com>
2021-03-12 00:16:08 -0500

    added new api end point for different front-end functionality

--- api/api.py
+++ api/api.py
@@ -27,3 +27,22 @@ def search():
                 else:
                     search_results[file_name] = [sentence]
     return search_results
+
+@app.route('/search2')
+def search2():
+    search_term = request.args.get('term').lower()
+    file_text = all_file_text()
+    search_results = {}
+    for file_name in file_text.keys():
+        text = file_text[file_name]
+        sentenctes = text.split('. ')
+        count = 0
+        for sentence in sentenctes:
+            if search_term in sentence.lower():
+                sentence_obj = {"start": count, "length": len(sentence)}
+                if file_name in search_results:
+                    search_results[file_name]["results"].append(sentence_obj)
+                else:
+                    search_results[file_name] = {"text": text, "results": [sentence_obj]}
+            count += len(sentence) + 2
+    return search_results
