43af1295407f41638ea82aa4636732cb2662ff5a
Sharan Arkalgud <sharkalgud@fb.com>
2021-03-12 01:44:11 -0500

    added smoothish scrolling to interface

--- src/ResultsContainer2.js
+++ src/ResultsContainer2.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import Result2 from './Result2'
 
 function ResultsContainer2(props) {
+    const resultsRef = useRef(null);
+    useEffect(() => {
+        var nodes = resultsRef.current.querySelectorAll("mark");
+        console.log(nodes.length);
+        if(nodes.length > 0){
+            nodes[0].scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+
     return (
-        <div>
+        <div ref={resultsRef}>
             {Object.entries(props.results).map(result => <Result2 result = {result}/>)}
         </div>
     );
