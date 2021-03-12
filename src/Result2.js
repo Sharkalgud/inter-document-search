031d0e227fe99f159b2222faccb2bcc63b16c213
Sharan Arkalgud <sharkalgud@fb.com>
2021-03-12 01:16:19 -0500

    adde basic toggling of text

--- src/Result2.js
+++ src/Result2.js
@@ -1,8 +1,9 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import Mark from "mark.js"
 
 function Result2(props){
     const textRef = useRef(null);
+    const [textShowing, setTextShowing] = useState(true)
 
     useEffect(() => {
         var markInstance = new Mark(textRef.current);
@@ -13,9 +14,18 @@ function Result2(props){
         });
     });
 
+    const toggleText = () => {
+        if(textShowing){
+            textRef.current.style.display = '';
+        } else {
+            textRef.current.style.display = 'none';
+        }
+        setTextShowing(!textShowing);
+    };
+
     return (
         <div>
-            <h1>{props.result[0]}</h1>
+            <h1 onClick={toggleText}>{props.result[0]}</h1>
             <p ref={textRef}>{props.result[1]["text"]}</p>
         </div>
     );
